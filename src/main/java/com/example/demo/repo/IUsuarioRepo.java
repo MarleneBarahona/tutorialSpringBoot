4208674963d5b3c4ecc0d08b73dc89fe3b54e663
MarleneBarahona <00118616@uca.edu.sv>
2020-07-22 18:17:11 -0600

    Agregando encriptado de Spring Secutiry a la Base (video 10). Se creo el Service de Usuario y el SecurityConfig.

--- src/main/java/com/example/demo/repo/IUsuarioRepo.java
+++ src/main/java/com/example/demo/repo/IUsuarioRepo.java
@@ -2,11 +2,13 @@ package com.example.demo.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.example.demo.model.Persona;
 import com.example.demo.model.Usuario;
 
 public interface IUsuarioRepo extends JpaRepository<Usuario, Integer> {
 
+	Usuario findByNombre(String username);
+
+	
 	
 	
 }
